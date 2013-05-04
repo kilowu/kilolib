000edc7d173b100710c214ec151c431d2064dc22
kilowu <canri62@gmail.com>
2013-05-04 15:38:10 +0800

    unique-triples

---
+++ sicp-ex-solution/ex-2.41.scm
@@ -0,0 +1,39 @@
+;; library functions
+(define nil ())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;; from ex-2.40
+(define (unique-pairs n)
+  (flatmap (lambda (x) (map (lambda (i) (list i x))
+                            (enumerate-interval 1 x)))
+           (enumerate-interval 1 n)))
+             
+(define (unique-triples n)
+  (flatmap (lambda (i) (map (lambda (j) (cons i j))
+                                (unique-pairs i)))
+           (enumerate-interval 1 n)))
+
+(unique-triples 3)
+
+(define (triples-sum-equal n s)
+  (filter (lambda (x) (= s
+                         (accumulate + 0 x)))
+          (unique-triples n)))
+
+(triples-sum-equal 3 7)
+                                
+                  
+                  
