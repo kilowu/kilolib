04a01314e8643b895d693116c80e997be339d13f
Wu Wei <weiwu@cacheme.net>
2018-12-26 21:50:00 +0800

    Adjust use package intialization code

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,16 +3,21 @@
 ;; Copyright (c) 2018
 
 
-;; MELPA setup
-(require 'package)
-(let* ((no-ssl (or (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; MELPA setup
+  (require 'package)
+  (let* ((no-ssl (or (memq system-type '(windows-nt ms-dos))
+                     (not (gnutls-available-p))))
+         (proto (if no-ssl "http" "https")))
+    (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+    (when (< emacs-major-version 24)
+      ;; For important compatibility libraries like cl-lib
+      (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+  (package-initialize)
+  (unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+  (require 'use-package))
 
 
 (set-language-environment 'UTF-8)
@@ -92,3 +97,21 @@
 (setq projectile-enable-caching t)
 
 (setq projectile-completion-system 'helm)
+
+
+;; Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (flycheck use-package projectile helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
