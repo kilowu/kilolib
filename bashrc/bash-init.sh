c560314bd1cb091cea8a0ca91cbf9f853c42ce57
Wu Wei <weiwu@cacheme.net>
2018-12-27 22:46:55 +0800

    Separate dotfiles to another repository

--- bashrc/bash-init.sh
+++
@@ -1,5 +0,0 @@
-# Why a separate file rather than .bashrc ?
-#   I may work in a environment that need to share a single linux account
-#   with other people. In that case, I cannot pollute the ~/.bashrc, and
-#   need to manually source this file alone.
-
